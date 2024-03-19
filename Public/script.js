ffa5bb23ecd76a2a52230d57ab92763a7279bc88
kimptgr <163475107+kimptgr@users.noreply.github.com>
2024-03-19 20:36:14 +0100

    Update script.js -aussi

--- Public/script.js
+++ Public/script.js
@@ -1,5 +1,5 @@
 carousel = ["./assets/image/chaussettecarousel.jpg", "./assets/image/escaladecarousel.jpg", "./assets/image/servonsupcarousel.jpg"];
-carouseltexte = ["<em>Je passe une grande partie de mon temps libre à créer : je couds, je tricote...</em>", "<em>J'aime aussi beaucoup bouger. En ce moment, je fais de l'escalade en blocs & de la musculation à la maison.</em>", "<em>Membre du bureau de l'association Servon sur un plateau, je joue à des jeux de société mais aussi de rôle.</em>"];
+carouseltexte = ["<em>Je passe une grande partie de mon temps libre à créer : je couds, je tricote...</em>", "<em>J'aime beaucoup bouger. En ce moment, je fais de l'escalade en blocs & de la musculation à la maison.</em>", "<em>Membre du bureau de l'association Servon sur un plateau, je joue à des jeux de société mais aussi de rôle.</em>"];
 carouselalt = ["Une paire de chaussettes tricotées", "Un mur d'escalade en salle", "Le logo de l'association Servon sur un plateau"]
 numimage = 1 ; 
 function appuieBouton(direction) {
